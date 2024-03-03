b92a1b29d60950397a88d1eb68d17ae08f3050a5
Deadstroke72 <161549447+Deadstroke72@users.noreply.github.com>
2024-03-03 17:47:49 -0600

    Update RegistroUsuario.java

--- ByteGear/src/itz/RegistroUsuario.java
+++ ByteGear/src/itz/RegistroUsuario.java
@@ -5,144 +5,159 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RegistroUsuario extends JFrame {
-	private String Nombre;
-	private String Correo;
-	private String Contraseña;
+    // Atributos para almacenar los datos del usuario
+    private String Nombre;
+    private String Correo;
+    private String Contraseña;
 
-	public RegistroUsuario() {
-		// TODO Auto-generated constructor stub
-	}
+    // Constructor por defecto
+    public RegistroUsuario() {
+        // TODO Auto-generated constructor stub
+    }
+
+    // Constructor con parámetros para inicializar los datos del usuario
     public RegistroUsuario(String nombre, String correo, String contraseña) throws HeadlessException {
-		super();
-		Nombre = nombre;
-		Correo = correo;
-		Contraseña = contraseña;
-	}
+        super();
+        Nombre = nombre;
+        Correo = correo;
+        Contraseña = contraseña;
+    }
 
+    // Métodos getter y setter para los atributos
+    public String getNombre() {
+        return Nombre;
+    }
 
-	public String getNombre() {
-		return Nombre;
-	}
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
 
-	public void setNombre(String nombre) {
-		Nombre = nombre;
-	}
+    public String getCorreo() {
+        return Correo;
+    }
 
-	public String getCorreo() {
-		return Correo;
-	}
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
 
-	public void setCorreo(String correo) {
-		Correo = correo;
-	}
+    public String getContraseña() {
+        return Contraseña;
+    }
 
-	public String getContraseña() {
-		return Contraseña;
-	}
+    public void setContraseña(String contraseña) {
+        Contraseña = contraseña;
+    }
 
-	public void setContraseña(String contraseña) {
-		Contraseña = contraseña;
-	}
+    // Método para solicitar los datos de registro al usuario
+    public void pedirDatos() {
+        JDialog datos = new JDialog();
+        datos.setTitle("Datos del Usuario");
+        datos.setModal(true);
+        datos.setLayout(new FlowLayout());
+        JLabel lblnombre = new JLabel("Usuario:");
+        JTextField txtNombre = new JTextField(30);
+        JLabel lblcarrera = new JLabel("Correo:");
+        JTextField txtCarrera = new JTextField(20);
+        JLabel lblsem = new JLabel("Contraseña:");
+        JTextField txtsem = new JTextField(5);
+        JButton aceptar = new JButton("Aceptar");
+        aceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Obtener los datos ingresados por el usuario
+                    Nombre = txtNombre.getText();
+                    Correo = txtCarrera.getText();
+                    Contraseña = txtsem.getText();
+                    datos.dispose();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error en datos");
+                    // Limpiar los campos en caso de error
+                    txtNombre.setText("");
+                    txtCarrera.setText("");
+                    txtsem.setText("");
+                }
+            }
+        });
+        JButton cancelar = new JButton("Cancelar");
+        cancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Establecer los datos como nulos y cerrar la ventana
+                Nombre = null;
+                datos.dispose();
+            }
+        });
+        datos.add(lblnombre);
+        datos.add(txtNombre);
+        datos.add(lblcarrera);
+        datos.add(txtCarrera);
+        datos.add(lblsem);
+        datos.add(txtsem);
+        datos.add(aceptar);
+        datos.add(cancelar);
+        datos.setSize(200, 150);
+        datos.setVisible(true);
+    }
 
-	public void pedirDatos() {
-	      JDialog datos=new JDialog();	
-	      datos.setTitle("Datos del Alumno");
-	      datos.setModal(true);
-	      datos.setLayout(new FlowLayout());
-	      JLabel lblnombre=new JLabel("Usuario:");
-	      JTextField txtNombre =new JTextField(30);
-	      JLabel lblcarrera=new JLabel("Correo:");
-	      JTextField txtCarrera =new JTextField(20);
-	      JLabel lblsem=new JLabel("Contraseña:");
-	      JTextField txtsem =new JTextField(5);
-	      JButton aceptar = new JButton("Aceptar");
-	      aceptar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-				Nombre=txtNombre.getText();
-				Correo=txtCarrera.getText();
-				Contraseña=txtsem.getText();
-				datos.dispose();
-				} catch (Exception ex) {
-				JOptionPane.showMessageDialog(null,"Error en datos");
-				txtNombre.setText("");
-				txtCarrera.setText("");
-				txtsem.setText("");
-				}
-			}  
-	      });
-	      JButton cancelar =new JButton("Cancelar");
-	      cancelar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Nombre=null;
-				datos.dispose();
-			}});
-	      datos.add(lblnombre);
-	      datos.add(txtNombre);
-	      datos.add(lblcarrera);
-	      datos.add(txtCarrera);
-	      datos.add(lblsem);
-	      datos.add(txtsem);
-	      datos.add(aceptar);
-	      datos.add(cancelar);
-	      datos.setSize(200, 150);
-	      datos.setVisible(true);
-	}
-	public void modificarDatos() {
-	      JDialog datos=new JDialog();	
-	      datos.setTitle("Modificar Datos del Alumno");
-	      datos.setModal(true);
-	      datos.setLayout(new FlowLayout());
-	      JLabel lblnombre=new JLabel("Usuario:");
-	      JTextField txtNombre =new JTextField(30);
-	      txtNombre.setText(Nombre);
-	      JLabel lblcarrera=new JLabel("Correo:");
-	      JTextField txtCarrera =new JTextField(20);
-	      txtCarrera.setText(Correo);
-	      JLabel lblsem=new JLabel("Contraseña:");
-	      JTextField txtsem =new JTextField(5);
-	      txtsem.setText(Contraseña);
-	      JButton aceptar = new JButton("Aceptar");
-	      aceptar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-				Nombre=txtNombre.getText();
-				Correo=txtCarrera.getText();
-				Contraseña=txtsem.getText();
-				datos.dispose();
-				} catch (Exception ex) {
-				JOptionPane.showMessageDialog(null,"Error en datos");
-				txtNombre.setText(Nombre);
-				txtCarrera.setText(Correo);
-				txtsem.setText(Contraseña);
-				}
-			}  
-	      });
-	      JButton cancelar =new JButton("Cancelar");
-	      cancelar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-			Nombre=null;
-				datos.dispose();
-			}});
-	      datos.add(lblnombre);
-	      datos.add(txtNombre);
-	      datos.add(lblcarrera);
-	      datos.add(txtCarrera);
-	      datos.add(lblsem);
-	      datos.add(txtsem);
-	      datos.add(aceptar);
-	      datos.add(cancelar);
-	      datos.setSize(400, 150);
-	      datos.setVisible(true);
-	}
-	@Override
-	public String toString() {
-		return Nombre + ","+ Correo+","+Contraseña+"\n"; //CSV
-	}
+    // Método para modificar los datos del usuario
+    public void modificarDatos() {
+        JDialog datos = new JDialog();
+        datos.setTitle("Modificar Datos del Usuario");
+        datos.setModal(true);
+        datos.setLayout(new FlowLayout());
+        JLabel lblnombre = new JLabel("Usuario:");
+        JTextField txtNombre = new JTextField(30);
+        txtNombre.setText(Nombre);
+        JLabel lblcarrera = new JLabel("Correo:");
+        JTextField txtCarrera = new JTextField(20);
+        txtCarrera.setText(Correo);
+        JLabel lblsem = new JLabel("Contraseña:");
+        JTextField txtsem = new JTextField(5);
+        txtsem.setText(Contraseña);
+        JButton aceptar = new JButton("Aceptar");
+        aceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Obtener los datos modificados por el usuario
+                    Nombre = txtNombre.getText();
+                    Correo = txtCarrera.getText();
+                    Contraseña = txtsem.getText();
+                    datos.dispose();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error en datos");
+                    // Restaurar los valores anteriores en caso de error
+                    txtNombre.setText(Nombre);
+                    txtCarrera.setText(Correo);
+                    txtsem.setText(Contraseña);
+                }
+            }
+        });
+        JButton cancelar = new JButton("Cancelar");
+        cancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Establecer los datos como nulos y cerrar la ventana
+                Nombre = null;
+                datos.dispose();
+            }
+        });
+        datos.add(lblnombre);
+        datos.add(txtNombre);
+        datos.add(lblcarrera);
+        datos.add(txtCarrera);
+        datos.add(lblsem);
+        datos.add(txtsem);
+        datos.add(aceptar);
+        datos.add(cancelar);
+        datos.setSize(400, 150);
+        datos.setVisible(true);
+    }
 
-	
+    // Método toString para convertir los datos del usuario en una cadena
+    @Override
+    public String toString() {
+        return Nombre + "," + Correo + "," + Contraseña + "\n"; // Formato CSV
+    }
 }
