337cf560977890348a82235071dfee0eb8db4166
Deadstroke72 <161549447+Deadstroke72@users.noreply.github.com>
2024-03-03 17:46:30 -0600

    Update ModelTable.java

--- ByteGear/src/itz/ModelTable.java
+++ ByteGear/src/itz/ModelTable.java
@@ -9,41 +9,52 @@ import java.awt.*;
 import java.util.List;
 
 public class ModelTable extends AbstractTableModel {
-	private List<RegistroUsuario> re;
-	private String[] columnas= {"Usuario ","Correo","Contraseña"};
-		 
-		public ModelTable(List<RegistroUsuario> re) {
-		 this.re= re;
-		}
-		public int getRowCount() {
-			// TODO Auto-generated method stub
-			return re.size();
-		}
+	import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
-		@Override
-		public int getColumnCount() {
-			// TODO Auto-generated method stub
-			return columnas.length;
-		}
-	public String getColumnName(int columna) {
-		return columnas[columna];
-	}
-		@Override
-		
-		public Object getValueAt(int fila, int columna) {
-			// TODO Auto-generated method stub
-			RegistroUsuario registro  = re.get(fila);
-			switch(columna) {
-			case 0:
-				return registro.getNombre();
-			case 1:
-				return registro.getCorreo();
-			case 2:
-				return registro.getContraseña();
-				default:
-					throw new IllegalArgumentException("Coliumna Invalida");
-			}
-		}
+public class ModelTable extends AbstractTableModel {
+    // Lista de objetos RegistroUsuario
+    private List<RegistroUsuario> re;
+    // Nombres de las columnas
+    private String[] columnas = {"Usuario", "Correo", "Contraseña"};
 
+    // Constructor que recibe la lista de objetos RegistroUsuario
+    public ModelTable(List<RegistroUsuario> re) {
+        this.re = re;
+    }
+
+    // Devuelve el número de filas en la tabla
+    @Override
+    public int getRowCount() {
+        return re.size();
+    }
+
+    // Devuelve el número de columnas en la tabla
+    @Override
+    public int getColumnCount() {
+        return columnas.length;
+    }
+
+    // Devuelve el nombre de la columna especificada
+    @Override
+    public String getColumnName(int columna) {
+        return columnas[columna];
+    }
+
+    // Devuelve el valor para la celda en la fila y la columna especificadas
+    @Override
+    public Object getValueAt(int fila, int columna) {
+        RegistroUsuario registro = re.get(fila);
+        switch (columna) {
+            case 0:
+                return registro.getNombre();
+            case 1:
+                return registro.getCorreo();
+            case 2:
+                return registro.getContraseña();
+            default:
+                throw new IllegalArgumentException("Columna Inválida");
+        }
+    }
 }
 
