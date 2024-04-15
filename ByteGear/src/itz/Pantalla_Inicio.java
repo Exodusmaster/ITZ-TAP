b8d3785759ade03205d5d9b4d23b98db0329f100
Dj0402 <161591744+Dj0402@users.noreply.github.com>
2024-04-14 18:26:22 -0600

    Update Pantalla_Inicio.java

--- ByteGear/src/itz/Pantalla_Inicio.java
+++ ByteGear/src/itz/Pantalla_Inicio.java
@@ -14,6 +14,8 @@ import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Pantalla_Inicio extends JFrame {
 
@@ -71,12 +73,12 @@ public class Pantalla_Inicio extends JFrame {
 		lblByte_Gear.setFont(new Font("Eras Bold ITC", Font.PLAIN, 80));
 		lblByte_Gear.setBounds(135, 0, 734, 91);
 		contentPane.add(lblByte_Gear);
-		
+		//Imagen Logo
 		JLabel lblLogoBG = new JLabel("");
 		lblLogoBG.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\BG.png"));
 		lblLogoBG.setBounds(50, 0, 80, 100);
 		contentPane.add(lblLogoBG);
-		
+		//Imagen Azul
 		JLabel lblAdorno1 = new JLabel("");
 		lblAdorno1.setBounds(0, 0, 869, 100);
 		lblAdorno1.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Adrono Azul.png"));
@@ -87,136 +89,180 @@ public class Pantalla_Inicio extends JFrame {
 		lblAdorno2.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Adrono Azul.png"));
 		lblAdorno2.setBounds(0, 505, 869, 51);
 		contentPane.add(lblAdorno2);
-		
+		//Menu
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setForeground(new Color(255, 255, 255));
 		menuBar.setBackground(new Color(0, 0, 64));
 		menuBar.setBounds(0, 102, 869, 44);
 		contentPane.add(menuBar);
-		
+		//Opciones
 		JMenu mnOpciones = new JMenu("Menu");
 		mnOpciones.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Menu.png"));
 		mnOpciones.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		menuBar.add(mnOpciones);
 		mnOpciones.setForeground(new Color(255, 255, 255));
 		mnOpciones.setBackground(new Color(0, 0, 64));
-		
+		//Regresa a inicio
+		JMenuItem mntmInicio = new JMenuItem("INICIO");
+		mntmInicio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		mntmInicio.setBackground(new Color(0, 0, 64));
+		mntmInicio.setForeground(Color.WHITE);
+		mntmInicio.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\BG32.png"));
+		mnOpciones.add(mntmInicio);
+		//Accesorios
 		JMenuItem mntmACCESORIOS = new JMenuItem("ACCESORIOS");
 		mntmACCESORIOS.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Mouse.png"));
 		mntmACCESORIOS.setBackground(new Color(0, 0, 64));
 		mntmACCESORIOS.setForeground(Color.WHITE);
 		mnOpciones.add(mntmACCESORIOS);
-		
+		//Computadoras
 		JMenuItem mntmCOMPUTADORAS = new JMenuItem("COMPUTADORAS");
 		mntmCOMPUTADORAS.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\monitor.png"));
 		mntmCOMPUTADORAS.setForeground(Color.WHITE);
 		mntmCOMPUTADORAS.setBackground(new Color(0, 0, 64));
 		mnOpciones.add(mntmCOMPUTADORAS);
-		
+		//Mantenimiento
 		JMenuItem mntmALMACENAMIENTO = new JMenuItem("ALMACENAMIENTO");
 		mntmALMACENAMIENTO.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\usb.png"));
 		mntmALMACENAMIENTO.setBackground(new Color(0, 0, 64));
 		mntmALMACENAMIENTO.setForeground(new Color(255, 255, 255));
 		mnOpciones.add(mntmALMACENAMIENTO);
-		
+		//Electronica de consumo
 		JMenuItem mntmELECTRONICACONSUMO = new JMenuItem("ELECTRONICA DE CONSUMO");
 		mntmELECTRONICACONSUMO.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\videogame.png"));
 		mntmELECTRONICACONSUMO.setBackground(new Color(0, 0, 64));
 		mntmELECTRONICACONSUMO.setForeground(Color.WHITE);
 		mnOpciones.add(mntmELECTRONICACONSUMO);
-		
-		JMenuItem mntmComponentes = new JMenuItem("Componentes ");
+		//Componentes 
+		JMenuItem mntmComponentes = new JMenuItem("COMPONENTES");
 		mntmComponentes.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Componentes.png"));
 		mntmComponentes.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		mntmComponentes.setForeground(new Color(255, 255, 255));
+		mntmComponentes.setForeground(Color.WHITE);
 		mntmComponentes.setBackground(new Color(0, 0, 64));
 		mnOpciones.add(mntmComponentes);
-		
+		//Impresoras
 		JMenuItem mntmIMPRESIÓN = new JMenuItem("IMPRESI\u00D3N");
 		mntmIMPRESIÓN.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\impresora.png"));
 		mntmIMPRESIÓN.setBackground(new Color(0, 0, 64));
 		mntmIMPRESIÓN.setForeground(Color.WHITE);
 		mnOpciones.add(mntmIMPRESIÓN);
-		
+		//Software
 		JMenuItem mntmSOFTWARE = new JMenuItem("SOFTWARE");
 		mntmSOFTWARE.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Software.png"));
 		mntmSOFTWARE.setBackground(new Color(0, 0, 64));
 		mntmSOFTWARE.setForeground(Color.WHITE);
 		mnOpciones.add(mntmSOFTWARE);
-		
+		//Monitores
 		JMenuItem mntmMONITORES = new JMenuItem("MONITORES");
 		mntmMONITORES.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\monitor.png"));
 		mntmMONITORES.setBackground(new Color(0, 0, 64));
 		mntmMONITORES.setForeground(Color.WHITE);
 		mnOpciones.add(mntmMONITORES);
-		
+		//Celulares
 		JMenuItem mntmCELULARES = new JMenuItem("CELULARES");
 		mntmCELULARES.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\telefono.png"));
 		mntmCELULARES.setBackground(new Color(0, 0, 64));
 		mntmCELULARES.setForeground(Color.WHITE);
 		mnOpciones.add(mntmCELULARES);
-		
+		//Energia
 		JMenuItem mntmENERGIA = new JMenuItem("ENERGIA");
 		mntmENERGIA.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Energia.png"));
 		mntmENERGIA.setBackground(new Color(0, 0, 64));
 		mntmENERGIA.setForeground(Color.WHITE);
 		mnOpciones.add(mntmENERGIA);
-		
+		//Menu Usuario
 		JMenu mnUsuario = new JMenu("Ingresa");
 		mnUsuario.setForeground(new Color(255, 255, 255));
 		mnUsuario.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		mnUsuario.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Usuario.png"));
 		menuBar.add(mnUsuario);
-		
+		//Ingresa (Abrir una nueva ventana para iniciar sesion)
 		JMenuItem mntmIngresa = new JMenuItem("Ingresa");
+		mntmIngresa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								Inicio_sesion frame = new Inicio_sesion();
+								frame.setVisible(true);
+								frame.setLocationRelativeTo(frame);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			
+		});
 		mntmIngresa.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Ingresa.png"));
 		mntmIngresa.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		mntmIngresa.setBackground(new Color(0, 0, 64));
 		mntmIngresa.setForeground(new Color(255, 255, 255));
 		mnUsuario.add(mntmIngresa);
-		
+		//Registra (Abrir una nueva ventana para registrar)
 		JMenuItem mntmRegistra = new JMenuItem("Registra");
+		mntmRegistra.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					EventQueue.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								Registro_Usuario frame = new Registro_Usuario();
+								frame.setVisible(true);
+								frame.setLocationRelativeTo(frame);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+			
+		});
 		mntmRegistra.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\registra.png"));
 		mntmRegistra.setForeground(Color.WHITE);
 		mntmRegistra.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		mntmRegistra.setBackground(new Color(0, 0, 64));
 		mnUsuario.add(mntmRegistra);
-		
+		//Carrito de compra
 		JMenu mnCompras = new JMenu("Mi bolsa");
 		mnCompras.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		mnCompras.setForeground(new Color(255, 255, 255));
 		mnCompras.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Compras.png"));
 		menuBar.add(mnCompras);
-		
+		//Ayuda
 		JMenu mnAyuda = new JMenu("Ayuda");
 		mnAyuda.setForeground(new Color(255, 255, 255));
 		mnAyuda.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		mnAyuda.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\Atencion.png"));
 		menuBar.add(mnAyuda);
-		
+		//Forma de comunicar con servicio
 		JMenuItem mntmContactos = new JMenuItem("Contacto");
 		mntmContactos.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\contacto.png"));
 		mntmContactos.setForeground(new Color(255, 255, 255));
 		mntmContactos.setBackground(new Color(0, 0, 64));
 		mnAyuda.add(mntmContactos);
-		
+		//pregunta frecuentes
 		JMenuItem mntmPreguntas_Frecuentes = new JMenuItem("Preguntas frecuentes ");
 		mntmPreguntas_Frecuentes.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\pregunta.png"));
 		mntmPreguntas_Frecuentes.setForeground(Color.WHITE);
 		mntmPreguntas_Frecuentes.setBackground(new Color(0, 0, 64));
 		mnAyuda.add(mntmPreguntas_Frecuentes);
-		
+		//informacion sobre nosotros
 		JMenuItem mntmNosotros = new JMenuItem("Nosotros ");
 		mntmNosotros.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\nosotros.png"));
 		mntmNosotros.setForeground(Color.WHITE);
 		mntmNosotros.setBackground(new Color(0, 0, 64));
 		mnAyuda.add(mntmNosotros);
-		
+		//Legal
 		JMenuItem mntmAvisoLegal = new JMenuItem("Aviso Legal");
 		mntmAvisoLegal.setIcon(new ImageIcon("C:\\Users\\Wendy\\eclipse-workspace\\Proyecto Byte Gear\\bin\\Imagenes\\aviso.png"));
 		mntmAvisoLegal.setForeground(Color.WHITE);
 		mntmAvisoLegal.setBackground(new Color(0, 0, 64));
 		mnAyuda.add(mntmAvisoLegal);
 	}
-
+	
 }
